51f24004d9bb5e0f6a144698e6973e5e5c20dc1c
Johan Thomsen <johan.thomsen@hotmail.com>
2025-08-20 20:47:14 +0200

    Change to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-echo Building frontend assets...
+#!/bin/bash
+
+echo "Building frontend assets..."
 docker compose -f docker-compose.yml -f docker-compose.prod.yml run --rm assets
 
-echo Recreating containers with latest code...
+echo "Recreating containers with latest code..."
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
 
-echo Deployment complete!
+echo "Deployment complete!"
